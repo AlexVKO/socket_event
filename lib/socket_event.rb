4262c372679a58606e51a4beb2d4552702b22eaf
Ale ∴ <ale@alexvko.com>
2021-09-05 10:34:08 -0300

    Boilerplate

---
+++ lib/socket_event.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "socket_event/version"
+
+module SocketEvent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/socket_event_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SocketEvent do
+  it "has a version number" do
+    expect(SocketEvent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
